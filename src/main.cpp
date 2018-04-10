6813b49547b20bab717140b5e242c1b8446ed0b3
DaveH <29776679+dherman516@users.noreply.github.com>
2018-04-10 14:56:04 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,7 @@ int main(int argc, char* argv[])
 
 			case 't': turnurl = optarg; break;
 			case 'S': localstunurl = optarg ? optarg : defaultlocalstunurl; stunurl = localstunurl; break;
-			case 's': localstunurl = NULL; 				  
-				  stunurl = optarg;
-				  std::cout << "Stun Server0:" << stunurl << std::endl; 				  
-				  break;
+			case 's': stunurl = optarg; break;
 			
 			case 'a': audioLayer = optarg ? (webrtc::AudioDeviceModule::AudioLayer)atoi(optarg) : webrtc::AudioDeviceModule::kDummyAudio; break;
 			case 'n': streamName = optarg; break;
